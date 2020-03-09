dfb0f9a8d6b5fa568037c240d11c124bcf7fec0d
Michael Soh <sohmc@users.noreply.github.com>
2020-03-08 21:25:44 -0400

    Added SMTP changes to use TLS

--- roundcube/config/config.inc.php
+++ roundcube/config/config.inc.php
@@ -2,7 +2,7 @@
     $config['db_dsnw'] = 'mysql://someuser:somepassword@172.17.0.1:3306/roundcubemail';
     $config['db_dsnr'] = '';
     $config['default_host'] = 'ssl://172.17.0.1:993';
-    $config['smtp_server'] = 'ssl://172.17.0.1:465';
+    $config['smtp_server'] = 'tls://172.17.0.1:587';
     $config['des_key'] = 'somekey';
     $config['temp_dir'] = '/tmp/roundcube-temp';
     $config['plugins'] = ['archive', 'zipdownload'];
@@ -10,6 +10,12 @@
     $config['log_driver'] = 'stdout';
     $config['skin'] = 'larry';
     $config['imap_conn_options'] = array(
+          'ssl' => array(
+            'verify_peer'  => false,
+            'peer_name'    => 'husker.mikesoh.com',
+          )
+    );
+    $config['smtp_conn_options'] = array(
           'ssl' => array(
             'verify_peer'  => false,
             'peer_name'    => 'husker.mikesoh.com',
